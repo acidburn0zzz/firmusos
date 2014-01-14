4dee78e562f60dc8db85532cdc66e3b4399984dc
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2014-01-14 13:06:59 +0100

    makefile toolchain setup

---
+++ kernel/def.h
@@ -0,0 +1,25 @@
+/*
+This file is part of FirmusOS.
+
+FirmusOS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FirmusOS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FirmusOS.  If not, see <http://www.gnu.org/licenses/>.
+
+    Giacomo Picchiarelli <gpicchiarelli@gmail.com>
+*/
+
+#ifndef DEF_H
+#define DEF_H
+
+#define VERSION 0.1
+
+#endif // DEF_H
